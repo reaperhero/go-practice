7565a4e6de32394fe6e6237e4838979a6babe989
manlu <manlu@dtstack.com>
2023-06-25 15:46:18 +0800

    chenqiangjun update

--- pkg/flag/string_test.go
+++ pkg/flag/string_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	serviceName = flag.String("name", "myun", "")
+	serviceName = flag.String("name", "m  yun", "")
 	cliAge      = flag.Int("age", 28, "Input Your Age")
 	boolflag    = flag.Bool("boolflag", false, "bool flag value")
 )
@@ -15,6 +15,7 @@ var (
 // go test -v -name="sadsada"
 func Test_flag_string_01(t *testing.T) {
 	flag.Parse()
+	fmt.Println(*serviceName)
 	last := flag.Arg(0) // 除了识别以为的第一个值
 	fmt.Println(last)
 }
